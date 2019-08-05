2fa8b43577b03752f64cfda9f881bb3cadeedc12
gekka <giovanni.santoni72@gmail.com>
2019-08-05 18:34:04 +0200

    First commit

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,16 @@
+/home/giovanni/Musica/1997 - Alice canta Battiato/01-Chan-son Egocentrique.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/02-Summer on a solitary beach.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/03-Prospettiva Nevski.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/04-Gli uccelli.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/05-I treni di Tozeur.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/06-Mal dAfrica.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/07-Unaltra vita.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/08-Il vento caldo dellestate.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/09-Per Elisa.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/10-Il re del mondo.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/11-Segnali di vita.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/12-Una notte speciale.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/13-Le aquile.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/14-Guerriglia urbana.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/15-Luna indiana.mp3
+/home/giovanni/Musica/1997 - Alice canta Battiato/16-Momento dozio.mp3
